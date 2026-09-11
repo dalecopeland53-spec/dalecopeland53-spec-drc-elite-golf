73556a7c45c513ae9a8907fa8d76fbb3a56d1a57
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-12 02:47:32 +1000

    Add course data for Capricorn Resort Championship Course
    This file contains the master geographical coordinate matrix for the Capricorn Resort Championship Course, including details for each hole.

---
+++ courseData.js
@@ -0,0 +1,25 @@
+// courseData.js - Master Geographical Coordinate Matrix for Elite Golf
+export const CAPRICORN_RESORT_COURSE = {
+  courseName: "Capricorn Resort - Championship Course",
+  totalPar: 72,
+  holes: [
+    { holeNumber: 1, par: 4, handicap: 5, imperialLen: "492 yds", metricLen: "450 m", teeBox: { latitude: -23.10420, longitude: 150.73110 }, greenCenter: { latitude: -23.10750, longitude: 150.73050 } },
+    { holeNumber: 2, par: 4, handicap: 11, imperialLen: "383 yds", metricLen: "350 m", teeBox: { latitude: -23.10780, longitude: 150.73040 }, greenCenter: { latitude: -23.11120, longitude: 150.72980 } },
+    { holeNumber: 3, par: 3, handicap: 17, imperialLen: "164 yds", metricLen: "150 m", teeBox: { latitude: -23.11150, longitude: 150.72950 }, greenCenter: { latitude: -23.11310, longitude: 150.72910 } },
+    { holeNumber: 4, par: 5, handicap: 1, imperialLen: "541 yds", metricLen: "495 m", teeBox: { latitude: -23.11340, longitude: 150.72900 }, greenCenter: { latitude: -23.11890, longitude: 150.72750 } },
+    { holeNumber: 5, par: 4, handicap: 9, imperialLen: "405 yds", metricLen: "370 m", teeBox: { latitude: -23.11920, longitude: 150.72740 }, greenCenter: { latitude: -23.12250, longitude: 150.72680 } },
+    { holeNumber: 6, par: 4, handicap: 7, imperialLen: "421 yds", metricLen: "385 m", teeBox: { latitude: -23.12280, longitude: 150.72670 }, greenCenter: { latitude: -23.12610, longitude: 150.72610 } },
+    { holeNumber: 7, par: 3, handicap: 15, imperialLen: "186 yds", metricLen: "170 m", teeBox: { latitude: -23.12640, longitude: 150.72600 }, greenCenter: { latitude: -23.12800, longitude: 150.72560 } },
+    { holeNumber: 8, par: 5, handicap: 3, imperialLen: "519 yds", metricLen: "475 m", teeBox: { latitude: -23.12830, longitude: 150.72550 }, greenCenter: { latitude: -23.13380, longitude: 150.72400 } },
+    { holeNumber: 9, par: 4, handicap: 13, imperialLen: "394 yds", metricLen: "360 m", teeBox: { latitude: -23.13410, longitude: 150.72390 }, greenCenter: { latitude: -23.13740, longitude: 150.72330 } },
+    { holeNumber: 10, par: 4, handicap: 6, imperialLen: "410 yds", metricLen: "375 m", teeBox: { latitude: -23.13770, longitude: 150.72320 }, greenCenter: { latitude: -23.14100, longitude: 150.72260 } },
+    { holeNumber: 11, par: 4, handicap: 12, imperialLen: "372 yds", metricLen: "340 m", teeBox: { latitude: -23.14130, longitude: 150.72250 }, greenCenter: { latitude: -23.14460, longitude: 150.72190 } },
+    { holeNumber: 12, par: 3, handicap: 18, imperialLen: "153 yds", metricLen: "140 m", teeBox: { latitude: -23.14490, longitude: 150.72180 }, greenCenter: { latitude: -23.14650, longitude: 150.72140 } },
+    { holeNumber: 13, par: 5, handicap: 2, imperialLen: "536 yds", metricLen: "490 m", teeBox: { latitude: -23.14680, longitude: 150.72130 }, greenCenter: { latitude: -23.15230, longitude: 150.71980 } },
+    { holeNumber: 14, par: 4, handicap: 10, imperialLen: "416 yds", metricLen: "380 m", teeBox: { latitude: -23.15260, longitude: 150.71970 }, greenCenter: { latitude: -23.15590, longitude: 150.71910 } },
+    { holeNumber: 15, par: 4, handicap: 8, imperialLen: "399 yds", metricLen: "365 m", teeBox: { latitude: -23.15620, longitude: 150.71900 }, greenCenter: { latitude: -23.15950, longitude: 150.71840 } },
+    { holeNumber: 16, par: 3, handicap: 16, imperialLen: "175 yds", metricLen: "160 m", teeBox: { latitude: -23.15980, longitude: 150.71830 }, greenCenter: { latitude: -23.16140, longitude: 150.71790 } },
+    { holeNumber: 17, par: 5, handicap: 4, imperialLen: "558 yds", metricLen: "510 m", teeBox: { latitude: -23.16170, longitude: 150.71780 }, greenCenter: { latitude: -23.16720, longitude: 150.71630 } },
+    { holeNumber: 18, par: 4, handicap: 14, imperialLen: "432 yds", metricLen: "395 m", teeBox: { latitude: -23.16750, longitude: 150.71620 }, greenCenter: { latitude: -23.17080, longitude: 150.71560 } }
+  ]
+};
